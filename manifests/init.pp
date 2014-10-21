6d3ad4b12cde70d0f76b8a8d0b2c8503e39ba824
Scott Barr <scott@barr.co.za>
2014-10-21 13:37:43 +0200

    Remove whitespaces from spamd options.

--- manifests/init.pp
+++ manifests/init.pp
@@ -666,7 +666,7 @@ class spamassassin(
   }
 
   if $service_enabled {
-    $extra_options = inline_template("<% if @spamd_username -%>-u <%= @spamd_username -%><% end -%> <% if @spamd_groupname -%>-g <%= @spamd_groupname -%><% end -%> -m <%= @spamd_max_children %><% if @spamd_min_children -%> --min-children=<%=@spamd_min_children -%><% end -%> -i <%= @spamd_listen_address %> -A <%= @spamd_allowed_ips %><% if @spamd_nouserconfig -%> --nouser-config<% end -%><% if @spamd_allowtell -%> --allow-tell<% end -%><% if @spamd_sql_config -%> -q<% end -%>")
+    $extra_options = inline_template("<% if @spamd_username -%>-u <%= @spamd_username -%> <% end -%><% if @spamd_groupname -%>-g <%= @spamd_groupname -%> <% end -%>-m <%= @spamd_max_children %><% if @spamd_min_children -%> --min-children=<%=@spamd_min_children -%><% end -%> -i <%= @spamd_listen_address %> -A <%= @spamd_allowed_ips %><% if @spamd_nouserconfig -%> --nouser-config<% end -%><% if @spamd_allowtell -%> --allow-tell<% end -%><% if @spamd_sql_config -%> -q<% end -%>")
 
     file_line { 'spamd_options' :
       path    => $spamd_options_file,
