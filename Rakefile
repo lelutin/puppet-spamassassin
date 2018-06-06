b60d94575200ef028d2ccb0d47222eaa73ffbab8
Scott Barr <gsbarr@gmail.com>
2018-06-06 11:31:52 +0200

    Meta facelift. Fix rspec

--- Rakefile
+++ Rakefile
@@ -1,15 +1,5 @@
 require 'puppetlabs_spec_helper/rake_tasks'
-require 'puppet-lint/tasks/puppet-lint'
-require 'puppet_blacksmith/rake_tasks'
+require 'puppet-syntax/tasks/puppet-syntax'
+require 'puppet_blacksmith/rake_tasks' if Bundler.rubygems.find_name('puppet-blacksmith').any?
 
-Rake::Task[:lint].clear # workaround https://github.com/rodjek/puppet-lint/issues/331
-PuppetLint.configuration.relative = true # https://github.com/rodjek/puppet-lint/pull/334
-PuppetLint::RakeTask.new :lint do |config|
-  config.pattern = 'manifests/**/*.pp'
-  config.disable_checks = ["80chars", "class_inherits_from_params_class","class_parameter_defaults","disable_documentation","disable_single_quote_string_with_variables"]
-  config.fail_on_warnings = true
-end
-
-Blacksmith::RakeTask.new do |t|
-  t.build = false # do not build the module nor push it to the Forge, just do the tagging [:clean, :tag, :bump_commit]
-end
+PuppetLint.configuration.send('relative')
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,28 +1,36 @@
+
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'rspec-puppet-facts'
+
+begin
+  require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
+rescue LoadError => loaderror
+  warn "Could not require spec_helper_local: #{loaderror.message}"
+end
+
+include RspecPuppetFacts
+
+default_facts = {
+  puppetversion: Puppet.version,
+  facterversion: Facter.version,
+}
+
+default_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml'))
+default_module_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml'))
+
+if File.exist?(default_facts_path) && File.readable?(default_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_facts_path)))
+end
+
+if File.exist?(default_module_facts_path) && File.readable?(default_module_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_module_facts_path)))
+end
 
 RSpec.configure do |c|
-  c.include PuppetlabsSpec::Files
-
+  c.default_facts = default_facts
   c.before :each do
-    # Ensure that we don't accidentally cache facts and environment
-    # between test cases.
-    Facter::Util::Loader.any_instance.stubs(:load_all)
-    Facter.clear
-    Facter.clear_messages
-
-    # Store any environment variables away to be restored later
-    @old_env = {}
-    ENV.each_key {|k| @old_env[k] = ENV[k]}
-
-    if Gem::Version.new(`puppet --version`) >= Gem::Version.new('3.5')
-      Puppet.settings[:strict_variables]=true
-    end
-    if ENV['PARSER']
-      Puppet.settings[:parser]=ENV['PARSER']
-    end
-  end
-
-  c.after :each do
-    PuppetlabsSpec::Files.cleanup
+    # set to strictest setting for testing
+    # by default Puppet runs at warning level
+    Puppet.settings[:strict] = :warning
   end
 end
